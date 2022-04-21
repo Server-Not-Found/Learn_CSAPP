de7e86f95f0b104dca5717ef3ff5c5e3859eef5d
Server-Not-Found <1620832173@qq.com>
2022-04-21 23:58:21 +0800

    unit test main

---
+++ refactorf/main_hardware.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+#include<headers/cpu.h>
+#include<headers/memory.h>
+#include<headers/common.h>
+
+static void TestString2Uint();
+
+int main()
+{
+    TestString2Uint()
+    return 0;
+}
+
+static void TestString2Uint()
+{
+    const char*nums[12] = {
+        "0",
+        "-0",
+        "0x0",
+        "1234",
+        "0x1234",
+        "0xabcd",
+        "-0xabcd",
+        "-1234",
+        "21473124"
+        "-1223451",
+        "0x800000000",
+        "0xffffffffffffffff"
+    };
+    for(int i=0;i<12;i++)
+    {
+        printf("%s => %lx\n",nums[i], string2uint(nums[i]));
+    }
+    string2uint();
+}
