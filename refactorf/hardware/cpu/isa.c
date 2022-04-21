264cb9268c60382eeb68e4f3ff2d0379202239e3
Server-Not-Found <1620832173@qq.com>
2022-04-21 23:59:26 +0800

    refactor

--- refactorf/hardware/cpu/isa.c
+++ refactorf/hardware/cpu/isa.c
@@ -1,14 +1,13 @@
-#include <string.h>
-
+#include<string.h>
+#include<headers/instruction.h>
 //传递操作数的函数
-static void parse_operand(const char *str, od_t *od, core_t *cr)
+static void parse_operand(const char *str, od_t *od)
 {
     /*
         str: 汇编指令的字符串
         od: 存储操作数的结构体指针
-        cr: 指定激活的cpu核数
     */    
-   od->type = EMPTY;
+   od->type = OD_EMPTY;
    od->imm = 0;
    od->scal = 0;
    od->reg1 = 0;
